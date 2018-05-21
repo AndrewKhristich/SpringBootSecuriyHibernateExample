5c4cde337f1141b6e8165dd9c56ef7677dbdd1da
andrew <tab111andrew@gmail.com>
2018-05-21 21:54:23 +0700

    added some description

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,13 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = {
         DataSourceAutoConfiguration.class,
         DataSourceTransactionManagerAutoConfiguration.class,
         HibernateJpaAutoConfiguration.class })
-public class DemoApplication implements CommandLineRunner {
+public class DemoApplication extends SpringBootServletInitializer implements CommandLineRunner {
 
 
     private UserService service;
@@ -23,6 +25,11 @@ public class DemoApplication implements CommandLineRunner {
     @Autowired
     public DemoApplication(UserService service) {
         this.service = service;
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DemoApplication.class);
     }
 
     public static void main(String[] args) {
